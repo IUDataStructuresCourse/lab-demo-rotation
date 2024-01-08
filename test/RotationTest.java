6514b2c3ce3ba71db40fb08d72149e2bd04d1b8c
Tianyu Chen <chentianyu@outlook.com>
2024-01-08 16:46:08 -0500

    auto select seed

--- test/RotationTest.java
+++ test/RotationTest.java
@@ -34,7 +34,7 @@ public class RotationTest {
     @Test
     public void test_rotation_random() {
         String test_description = "rotating an array with random integers";
-        Random r = new Random(0);
+        Random r = new Random();
         int[] A = new int[100];
         for (int i = 0; i != A.length; ++ i) {
             A[i] = r.nextInt();
